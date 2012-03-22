ab2cd66e540bfa2a99db5a7351314765386caa99
Roberto <roberto@MiMacBookPro.local>
2012-03-23 00:39:03 +0100

    Added PubsQuerier utility class to query publications generated by PubsRDFizer Added SWRC class for the SWRC Ontology namespace

---
+++ src/main/java/net/rhizomik/grecrdfizer/SWRC.java
@@ -0,0 +1,10 @@
+package net.rhizomik.grecrdfizer;
+
+/**
+ * @author http://rhizomik.net/~roberto/
+ */
+public class SWRC {
+    private static String ns = "http://swrc.ontoware.org/ontology#";
+    public static String Article = ns+"Article";
+
+}
