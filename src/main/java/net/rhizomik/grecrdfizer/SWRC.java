3817c7911ea771eb869d4bf82d6a5b61ea6fba40
Roberto García <rogargon@gmail.com>
2012-06-28 18:47:50 +0200

    Added method listArticles that performs SELECT query and returns a result set serialised as XML to preserve order

--- src/main/java/net/rhizomik/grecrdfizer/SWRC.java
+++ src/main/java/net/rhizomik/grecrdfizer/SWRC.java
@@ -6,5 +6,6 @@ package net.rhizomik.grecrdfizer;
 public class SWRC {
     private static String ns = "http://swrc.ontoware.org/ontology#";
     public static String Article = ns+"Article";
-
+    public static String pages = ns+"pages";
+    public static String authors = ns+"authors";
 }
